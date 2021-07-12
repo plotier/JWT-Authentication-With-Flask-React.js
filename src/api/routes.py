61d3451b2d782f1daa8acf3bbc782d76f7ab9ecd
plotier <plotier15@gmail.com>
2021-07-12 10:54:54 +0000

    me perdí

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
-
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -15,7 +17,7 @@ def handle_hello():
         "message": "Hello! I'm a message that came from the backend"
     }
 
-    return jsonify(response_body), 200
+     return jsonify(response_body), 200
 
 @api.route('/sign_up', methods=['POST'])
 def sign_up_user():
@@ -30,5 +32,14 @@ def sign_up_user():
     user1 = User(name=name, last_name=last_name, email=email, password=password)
     db.session.add(user1)
     db.session.commit()
+     return jsonify({"msg": "El usuario ha sido creado exitosamente :) :O :O !!!"}), 200
+    
+    @app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+     return jsonify({"msg": "Bad username or password"}), 401
 
-    return jsonify({"msg": "El usuario ha sido creado exitosamente :) :O :O !!!"}), 200 
+    access_token = create_access_token(identity=username)
+     return jsonify(access_token=access_token)
