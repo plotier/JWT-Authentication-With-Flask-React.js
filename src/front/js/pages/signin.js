cc7b77439e4ce5c4d9c565cdfb5dc02e9db6fc43
plotier <plotier15@gmail.com>
2021-09-12 10:42:39 +0000

    half done

--- src/front/js/pages/signin.js
+++ src/front/js/pages/signin.js
@@ -1,8 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
+import { useHistory } from "react-router";
+import { Context } from "../store/appContext";
 import React from "react";
 
 export const Signin = () => {
+    const {store, actions}=useContext(Context)
+    const [eachEntry, setEachEntry]=useState(initialInputState);
+    const initialInputState={email="", password=""};
+    const handleInputChange = e => {
+		setEachEntry({ ...eachEntry, [e.target.name]: e.target.value });
+	};
     return(
-
-    )
+        <form onSubmit={handleFinalSubmit}>
+        <div className="form-group">
+            <label htmlFor="exampleInputEmail">Email address</label>
+            <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail"
+                placeholder="Enter email"
+                name="email"
+                onChange={handleInputChange}
+            />
+        </div>
+        <div className="form-group">
+            <label htmlFor="exampleInputPassword">Password</label>
+            <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword"
+                placeholder="Enter your password"
+                name="password"
+                onChange={handleInputChange}
+            />
+        </div>
+        <button type="submit" className="btn btn-primary float-right">
+            Submit
+        </button>
+    </form>
+    );
 };
